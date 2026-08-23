3ee733c2a3d88f80c4583ecce58d37daba14f5ca
yogesh ingavale <yogeshingavale2@gmail.com>
2026-08-23 20:41:55 +0530

    Added another project using Terraform

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -112,6 +112,17 @@ const projects = [
   },
 
 
+     {
+    id: 11,
+    title: "Terraform AWS S3 Static Website",
+    description:
+      "Built a static website hosting infrastructure on AWS S3 using Terraform. The complete S3 configuration, permissions, bucket policy, and website hosting settings were managed through Terraform instead of manual AWS Console configuration.",
+    image: "/projects/project-111.png",
+    tags: ["Terraform", "AWS S3", "Static Website", "Infrastructure as Code", "Automation"],
+    demoUrl: "#",
+    githubUrl: "https://github.com/Yogesh285/Terraform-AWS-S3-Static-Website.git",
+  },
+
   
   // {
   //   id: 3,
